aa4c26d87a1f5fd66ab68ef8b4e7135f82e77866
yacinekedidi <kedidiyacine@gmail.com>
2020-08-12 13:36:50 +0100

    setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- first 3 students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
