d54bfba2d2ae7e2248873da13d993d2d036a66a7
yacinekedidi <kedidiyacine@gmail.com>
2020-08-17 17:27:24 +0100

    new

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,14 @@ import os.path
 
 
 def do_pack():
-	"""function"""
-	dt = datetime.now()
-	arch_name = "versions/web_static_{}.tgz".format(dt.strftime("%Y%m%d%H%M%S"))
-	if not os.path.exists("versions/"):
-		os.mkdir("versions")
-	local("tar -cvzf {} web_static".format(arch_name))
-	if os.path.exists("{}".format(arch_name)):
-		return arch_name
-	else:
-		return None
+    """function"""
+    dt = datetime.now()
+    arch_name = "versions/web_static_{}.tgz"
+    .format(dt.strftime("%Y%m%d%H%M%S"))
+    if not os.path.exists("versions/"):
+        os.mkdir("versions")
+    local("tar -cvzf {} web_static".format(arch_name))
+    if os.path.exists("{}".format(arch_name)):
+        return arch_name
+    else:
+        return None
