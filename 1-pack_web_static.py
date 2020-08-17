44980dc8483b6660dbb730e6d6fd5bd27502ffe1
yacinekedidi <kedidiyacine@gmail.com>
2020-08-17 17:20:09 +0100

    new

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module"""
+from fabric.api import local
+from datetime import datetime
+import os.path
+
+
+def do_pack():
+	"""function"""
+	dt = datetime.now()
+	arch_name = "versions/web_static_{}.tgz".format(dt.strftime("%Y%m%d%H%M%S"))
+	if not os.path.exists("versions/"):
+		os.mkdir("versions")
+	local("tar -cvzf {} web_static".format(arch_name))
+	if os.path.exists("{}".format(arch_name)):
+		return arch_name
+	else:
+		return None
